093d01d1cb3414d3a7f38542a8bc82e509e1158c
gowrishankar2705 <141017922+gowrishankar2705@users.noreply.github.com>
2025-01-04 21:00:31 +0530

    Add files via upload

---
+++ generated/source/buildConfig/androidTest/debug/mooc/vandy/java4android/diamonds/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mooc.vandy.java4android.diamonds.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mooc.vandy.java4android.m4_diamonds.test";
+  public static final String BUILD_TYPE = "debug";
+}
